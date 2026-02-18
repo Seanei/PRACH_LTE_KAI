331213d3b4614982f877dd739228172ceae540d9
Vlad <gerdoexx@gmail.com>
2026-02-18 03:15:12 +0300

    fix, refactor: Not enough cyclic shifts to produce all preambles First version of the block was ready-to-go with one exact flow - it didn't generate all preambles to go with and did calculate cyclic shifts a bit off. Now block offers functions to generate cyclic shifts for restricted and unrestricted sets and all 64 preambles for given configuration

--- prach/blocks/ue/preamble_generator.py
+++ prach/blocks/ue/preamble_generator.py
@@ -139,20 +139,118 @@ U_ZC_TDD = [
 
 @BlockRegistry.register
 class PreambleGeneratorBlock(Block):
-    root_sequence_index = 0
-    preamble_format = 0
-    zero_correlation_config = 0
-    high_speed_flag = 0
+    root_sequence_index: int = 0
+    preamble_format: int = 0
+    preamble_index: int = -1
+    zero_correlation_config: int = 0
+    high_speed_flag: int = 0
 
     _random_system = random.SystemRandom()
 
+    def _generate_preambles(
+        self,
+        root_sequence_index: int,
+        zero_correlation_config: int,
+        high_speed_flag: int,
+        n_zc: int,
+        u_zc_table: list[int],
+        n_cs_table: list[list[int]],
+        total_preambles: int = 64,
+    ) -> list[list[complex]]:
+        n_cs = n_cs_table[zero_correlation_config][high_speed_flag]
+        preambles = []
+        current_root_index = root_sequence_index
+
+        while len(preambles) < total_preambles:
+            u_zc = u_zc_table[current_root_index]
+            base_seq = zadoff_chu(u_zc, n_zc)
+
+            shifts = self._get_shifts(n_zc, n_cs, u_zc * high_speed_flag)  # hs = 0 or 1
+
+            for c_v in shifts:
+                if len(preambles) >= total_preambles:
+                    break
+                c = c_v % n_zc
+                shifted_seq = base_seq[c:] + base_seq[:c]
+                preambles.append(shifted_seq)
+
+            current_root_index += 1
+            current_root_index = current_root_index % len(u_zc_table)
+
+        if len(preambles) < total_preambles:
+            # TODO: handle as runtime error?
+            pass
+        return preambles
+
+    def _get_shifts(self, n_zc: int, n_cs: int, u_zc: int = 0) -> list[int]:
+        return (
+            self._get_shifts_unrestricted(n_zc, n_cs)
+            if u_zc == 0
+            else self._get_shifts_restricted(n_zc, n_cs, u_zc)
+        )
+
+    # TODO: cache it?
+    def _get_shifts_unrestricted(self, n_zc: int, n_cs: int) -> list[int]:
+        if n_cs == 0:
+            return [0]
+
+        num_shifts = math.floor(n_zc / n_cs)
+        return [v * n_cs for v in range(num_shifts)]
+
+    # TODO: cache it?
+    def _get_shifts_restricted(self, n_zc: int, n_cs: int, u_zc: int) -> list[int]:
+        if n_cs == 0:
+            return [0]
+
+        # (p * u_zc) mod n_zc == 1
+        p = 1
+        while (p * u_zc) % n_zc != 1:
+            p += 1
+
+        d_u = p if p < n_zc / 2 else n_zc - p
+
+        if d_u >= n_zc / 3:
+            if d_u > (n_zc - n_cs) / 2:
+                return []
+
+            n_shift = math.floor((n_zc - 2 * d_u) / n_cs)
+            d_start = n_zc - 2 * d_u + n_shift * n_cs
+            n_group = math.floor(d_u / d_start)
+            n_shift_avg = min(
+                max(math.floor((d_u - n_group * d_start) / n_cs), 0),
+                n_shift,
+            )
+        elif n_cs <= d_u:
+            n_shift = math.floor(d_u / n_cs)
+            d_start = 2 * d_u + n_shift * n_cs
+            n_group = math.floor(n_zc / d_start)
+            n_shift_avg = max(
+                math.floor((n_zc - 2 * d_u - n_group * d_start) / n_cs), 0
+            )
+        else:
+            # no preamble from this root in restricted mode
+            return []
+
+        total_shifts = n_shift * n_group + n_shift_avg
+        shifts = []
+        for v in range(total_shifts):
+            c_v = d_start * math.floor(v / n_shift) + (v % n_shift) * n_cs
+            shifts.append(c_v)
+
+        return shifts
+
     def process(self, data: CommonData) -> CommonData:
+        # TODO: add 2 another functions to override for `Block`
+        #       to extract configuration variables from `CommonData`
+        #       and to validate it before processing
+
         # get higher levels configuration
         # use local configuration instead if none is present
         self.root_sequence_index = data.meta.get(
             "root_sequence_index", self.root_sequence_index
         )
         self.preamble_format = data.meta.get("preamble_format", self.preamble_format)
+        self.preamble_index = data.meta.get("preamble_index", self.preamble_index)
         self.zero_correlation_config = data.meta.get(
             "zero_correlation_config", self.zero_correlation_config
         )
@@ -162,66 +260,23 @@ class PreambleGeneratorBlock(Block):
 
         assert fdd, "TDD is currently unsupported"
 
-        n_zc = N_ZC_FDD
-        n_cs = N_CS_FDD[self.zero_correlation_config][self.high_speed_flag]
-        u_zc = U_ZC_FDD[self.root_sequence_index]  # logical root to physical
+        preamble_index = self.preamble_index
 
-        c_v = 0
+        if preamble_index == -1:
+            preamble_index = self._random_system.randrange(64)
 
-        if self.high_speed_flag == 0:
-            if n_cs == 0:
-                c_v = 0
-            else:
-                v = self._random_system.randrange(math.floor(n_zc / n_cs))
-                c_v = v * n_cs
-        else:
-            while self.root_sequnce_index < n_zc:
-                p = 0
-                while (p * u_zc) % n_zc != 1:
-                    p += 1
+        all_preambles = self._generate_preambles(
+            self.root_sequence_index,
+            self.zero_correlation_config,
+            self.high_speed_flag,
+            N_ZC_FDD,
+            U_ZC_FDD,
+            N_CS_FDD,
+            preamble_index + 1,  # small optimization :)
+        )
 
-                d_u = p if (0 <= p and p < n_zc / 2) else n_zc - p
+        selected_preamble = all_preambles[preamble_index]
 
-                n_shift = 0
-                d_start = 0
-                n_group = 0
-                n_shift_avg = 0
-
-                if n_cs <= d_u and d_u < n_zc / 3:
-                    n_shift = math.floor(d_u / n_cs)
-                    d_start = 2 * d_u + n_shift * n_cs
-                    n_group = math.floor(n_zc / d_start)
-                    n_shift_avg = max(
-                        math.floor((n_zc - 2 * d_u - n_group * d_start) / n_cs), 0
-                    )
-                elif n_zc / 3 <= d_u and d_u <= (n_zc - n_cs) / 2:
-                    n_shift = math.floor((n_zc - 2 * d_u) / n_cs)
-                    d_start = n_zc - 2 * d_u + n_shift * n_cs
-                    n_group = math.floor(d_u / d_start)
-                    n_shift_avg = min(
-                        max(math.floor((d_u - n_group * d_start) / n_cs), 0), n_shift
-                    )
-                else:
-                    # Additional preamble sequences, in case 64 preambles cannot be
-                    # generated from a single root Zadoff-Chu sequence, are obtained
-                    # from the root sequences with the consecutive logical indexes until
-                    # all the 64 sequences are found
-
-                    self.root_sequnce_index += 1
-                    u_zc = U_ZC_FDD[self.root_sequence_index]
-
-                    continue
-
-            v = self._random_system.randrange(n_shift * n_group + n_shift_avg - 1)
-            c_v = d_start * math.floor(v / n_shift) + (v % n_shift) * n_cs
-
-        zadoff_chu_seq = zadoff_chu(n_zc, u_zc)
-
-        # perform the cyclic shift
-        for i in range(c_v):
-            zadoff_chu_seq.append(zadoff_chu_seq.pop(0))
-
-        data.meta["root_sequence_index"] = self.root_sequence_index
-        data.meta["generated_preamble"] = zadoff_chu_seq
+        data.meta["generated_preamble"] = selected_preamble
 
         return data
