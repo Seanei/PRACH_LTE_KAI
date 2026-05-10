afef5fc5a4f9f5eef7532b3ff2dadc68c417e497
aisylu <ais.kkyr@gmail.com>
2026-05-10 18:16:38 +0300

    Implement NumPy-based PRACH subcarrier mapping

--- prach/blocks/ue/subcarrier_mapping.py
+++ prach/blocks/ue/subcarrier_mapping.py
@@ -1,43 +1,51 @@
+import numpy as np
+
 from prach.pipeline import CommonData, Block, BlockRegistry
 
-N_RB_SC = 12 #the number of subcarriers in one resource block;
-F_S = 30_720_000 #Hz sampling rate
-N_ZC_FDD = 839 # format 0-3, length of the Zadoff-Chu sequence
+N_RB_SC = 12                 # Number of subcarriers in one LTE resource block
+F_S = 30_720_000             # LTE sampling frequency, Hz
+N_ZC_FDD = 839               # PRACH Zadoff-Chu sequence length for formats 0-3
 
 
 @BlockRegistry.register
 class SubcarrierMappingBlock(Block):
-	n_ul_rb : int = 6#Uplink bandwidth configuration {6; 15; 25; 50; 75; 100}
-	phi: int = 7# fixed offset for formats 0-3
+    n_ul_rb: int = 100              # Uplink LTE bandwidth configuration
+    phi: int = 7                    # Standardized PRACH frequency offset
+    n_ra_prb_offset: int = 0       # PRACH resource block offset
 
-	n_ra_prb_offset: int = 0#RB offset to accommodate PRACH
+    delta_f_ra: int = 1250          # PRACH subcarrier spacing, Hz
+    delta_f: int = 15_000           # LTE uplink subcarrier spacing, Hz
 
-	delta_f_ra: int = 1250 #interval between adjacent subcarriers in PRACH, format0-3
-	delta_f: int = 15_000 #interval between adjacent subcarriers in LTE
+    def process(self, data: CommonData) -> CommonData:
+        self.n_ul_rb = data.meta.get("n_ul_rb", self.n_ul_rb)  
+        self.n_ra_prb_offset = data.meta.get(
+            "n_ra_prb_offset", self.n_ra_prb_offset
+        )  
 
-	def process(self, data: CommonData) -> CommonData:
-		self.n_ul_rb = data.meta.get('n_ul_rb', self.n_ul_rb) #pulling out the values
-		self.n_ra_prb_offset = data.meta.get('n_ra_prb_offset', self.n_ra_prb_offset) #pulling out the values
-		
-		frequencies = data.meta.get("dft", []) #we get the array after dft
+        # Input PRACH spectrum after DFT
+        frequencies = np.array(
+            data.meta.get("dft", []),
+            dtype=complex
+        )
 
-		K = self.delta_f/self.delta_f_ra#Zoom level - коэф масштабирования
-		n_fft = int(F_S / self.delta_f_ra) # output array size (24_576)
-		n_ra_prb = self.n_ra_prb_offset
-		#k0 = N_RB_SC * (n_ra_prb - self.n_ul_rb / 2)
-		k0 = n_ra_prb*N_RB_SC - self.n_ul_rb* N_RB_SC/2 ##
-		k_start = int(n_fft // 2 + self.phi + K * (k0 +0.5))
-		##first = self.phi + K * (k0 + 0.5) + 1 # from MATLAB
-		##k_start = int(n_ifft // 2 + first)
-		
-		spectrum = [0j] * int(n_fft)
+        # Frequency scaling factor
+        K = self.delta_f // self.delta_f_ra
 
-		for m in range(N_ZC_FDD):
-			i = k_start + m
-			if 0<=i<len(spectrum):
-				spectrum[i] = frequencies[m]
-			else:
-				pass
-		data.meta["Subcarrier_Mapping"] = spectrum
+        # Size of global LTE frequency grid
+        n_fft = int(F_S / self.delta_f_ra)
 
-		return data 
+        # Relative PRACH offset from central carrier
+        k0 = ( self.n_ra_prb_offset * N_RB_SC - (self.n_ul_rb * N_RB_SC) / 2 )
+
+        # Start index of PRACH placement
+        k_start = int( n_fft // 2 + self.phi + K * (k0 + 0.5) )
+
+        # Initialize LTE spectrum
+        spectrum = np.zeros(n_fft, dtype=complex)
+
+        # Insert PRACH subcarriers into LTE grid
+        spectrum[k_start:k_start + N_ZC_FDD] = frequencies
+
+        data.meta["Subcarrier_Mapping"] = spectrum
+
+        return data
