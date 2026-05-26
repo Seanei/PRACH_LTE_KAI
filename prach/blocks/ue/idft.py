d0b9496766e9f1c5114bc2eeaa5e28d804c0f769
Fretorian <katvad28@gmail.com>
2026-05-26 08:48:32 +0300

    Made Tests for IDFT + changed structure for IDFT Block

--- prach/blocks/ue/idft.py
+++ prach/blocks/ue/idft.py
@@ -4,8 +4,8 @@ from prach.math import idft
 
 @BlockRegistry.register
 class IDFTBlock(Block):
-        def process(data:list):
-                data.meta.get("Before_IDFT")
-                result = idft(data)
-                data.meta["After_IDFT"] = result
-                return data
+    def process(data: list):
+        data.meta.get("Before_IDFT")
+        result = idft(data)
+        data.meta["After_IDFT"] = result
+        return data
