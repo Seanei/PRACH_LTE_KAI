686c880ea57300bf27932c8b646ec59cda0cc090
aisylu <ais.kkyr@gmail.com>
2026-05-29 13:33:23 +0300

    Resolve merge conflict in ue package exports

--- prach/blocks/ue/__init__.py
+++ prach/blocks/ue/__init__.py
@@ -1,11 +1,9 @@
 from .preamble_generator import PreambleGeneratorBlock
 from .subcarrier_mapping import SubcarrierMappingBlock
-
+from .idft import IDFTBlock
 
 __all__ = [
     "PreambleGeneratorBlock",
     "SubcarrierMappingBlock",
+    "IDFTBlock",
 ]
-from .idft import IDFTBlock
-
-__all__ = ["PreambleGeneratorBlock", "IDFTBlock"]
