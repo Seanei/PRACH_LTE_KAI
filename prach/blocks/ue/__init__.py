608f9ea58c13fb0d35f9f02d66679090b54ae387
Sceplenie <153276732+Sceplenie@users.noreply.github.com>
2026-06-03 12:34:18 +0400

    Add DFT function (#30)
    * Add DFT block and test

--- prach/blocks/ue/__init__.py
+++ prach/blocks/ue/__init__.py
@@ -1,9 +1,6 @@
 from .preamble_generator import PreambleGeneratorBlock
 from .subcarrier_mapping import SubcarrierMappingBlock
 from .idft import IDFTBlock
+from .dft import DFTBlock
 
-__all__ = [
-    "PreambleGeneratorBlock",
-    "SubcarrierMappingBlock",
-    "IDFTBlock",
-]
+__all__ = ["PreambleGeneratorBlock", "IDFTBlock", "SubcarrierMappingBlock", "DFTBlock"]
