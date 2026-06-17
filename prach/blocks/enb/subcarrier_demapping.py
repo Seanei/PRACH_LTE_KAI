ef2d4f8cca6bfb63f75bcdb4a6b99710459fa999
aisylu <ais.kkyr@gmail.com>
2026-06-17 09:01:43 +0300

    Fix PEP8 issues in subcarrier demapping block

--- prach/blocks/enb/subcarrier_demapping.py
+++ prach/blocks/enb/subcarrier_demapping.py
@@ -2,20 +2,19 @@ import numpy as np
 
 from prach.pipeline import CommonData, Block, BlockRegistry
 
-N_RB_SC = 12
-F_S = 30_720_000
-N_ZC_FDD = 839
+N_RB_SC = 12  # Number of subcarriers in one LTE resource block
+F_S = 30_720_000  # LTE sampling frequency, Hz
+N_ZC_FDD = 839  # PRACH Zadoff-Chu sequence length for formats 0-3
 
 
 @BlockRegistry.register
 class SubcarrierDemappingBlock(Block):
+    n_ul_rb: int = 100  # Uplink LTE bandwidth configuration
+    phi: int = 7  # Standardized PRACH frequency offset
+    n_ra_prb_offset: int = 0  # PRACH resource block offset
 
-    n_ul_rb: int = 100
-    phi: int = 7
-    n_ra_prb_offset: int = 0
-
-    delta_f_ra: int = 1250
-    delta_f: int = 15000
+    delta_f_ra: int = 1250  # PRACH subcarrier spacing, Hz
+    delta_f: int = 15_000  # LTE uplink subcarrier spacing, Hz
 
     def process(self, data: CommonData) -> CommonData:
         # Load configurable parameters
@@ -29,10 +28,7 @@ class SubcarrierDemappingBlock(Block):
             self.n_ra_prb_offset
         )
         # Input LTE FFT spectrum
-        spectrum = np.array(
-            data.meta.get("fft", []),
-            dtype=complex
-        )
+        spectrum = np.array(data.meta.get("fft", []), dtype=complex)
 
         # Frequency scaling factor
         K = self.delta_f // self.delta_f_ra
@@ -40,16 +36,13 @@ class SubcarrierDemappingBlock(Block):
         n_fft = int(F_S / self.delta_f_ra)
 
         # Relative PRACH offset from central carrier
-        k0 = (
-            self.n_ra_prb_offset * N_RB_SC
+        k0 = ( self.n_ra_prb_offset * N_RB_SC
             - (self.n_ul_rb * N_RB_SC) / 2
         )
 
         # Start index of PRACH extraction
         k_start = int(
-            n_fft // 2
-            + self.phi
-            + K * (k0 + 0.5)
+            n_fft // 2 + self.phi + K * (k0 + 0.5)
         )
 
         prach_bins = spectrum[
