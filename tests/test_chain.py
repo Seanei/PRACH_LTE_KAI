f5b5d8477e3c5676c0236b71e883295b1c730847
Liya_Faizrakhmanova <cupcake0505@mail.ru>
2026-07-03 08:17:17 +0300

    fix style

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -55,7 +55,7 @@ output_windows = result.meta.get("prach_windows")
 print("\n--- Результаты mapper-теста ---")
 if output_windows:
     extracted_sf, extracted_sequence = output_windows[0]
-    print(f"Сигнал успешно передан в субфрейме: {extracted_sf}")
+    print(f"Сигнал успешно передан в сабфрейме: {extracted_sf}")
     
     max_diff = np.max(np.abs(reference_sequence - extracted_sequence))
     print(f"Максимальное различие отсчетов: {max_diff}")
