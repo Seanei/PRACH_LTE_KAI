23aaba9eddec9e35a43099518508ca5c2029977c
Liya_Faizrakhmanova <cupcake0505@mail.ru>
2026-07-03 08:23:12 +0300

    fix style

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -1,9 +1,6 @@
-from dataclasses import dataclass, field
-from typing import Dict, Any
 import numpy as np
-
 from prach.pipeline import Pipeline, CommonData
-from prach.blocks.ue.subframe_mapping import SubframeMappingBlock
+from prach.blocks.ue.subframe_mapping import SubframeMappingBlock 
 from prach.blocks.enb.subframe_demapping import SubframeDemappingBlock
 
 cfg = {
@@ -29,7 +26,7 @@ data = CommonData()
 data.meta = {}
 
 F_S = 30_720_000
-samples_per_subframe = int(F_S * 1e-3)  # 30720 отсчетов
+samples_per_subframe = int(F_S * 1e-3)
 cp_length = 3168
 sequence_length = 24576
 
@@ -47,19 +44,17 @@ data.meta["sf_n"] = 0
 data.meta["config_index"] = 0
 data.meta["preamble_format"] = 0
 
+print("Запуск пайплайна ")
 result = pipeline.run(data)
 
 output_windows = result.meta.get("prach_windows")
 
-print("\n Результаты mapper-теста")
 if output_windows:
     extracted_sf, extracted_sequence = output_windows[0]
     print(f"Сигнал успешно передан в сабфрейме: {extracted_sf}")
-    
     max_diff = np.max(np.abs(reference_sequence - extracted_sequence))
     print(f"Максимальное различие отсчетов: {max_diff}")
-    
     assert max_diff < 1e-12, "Ошибка: Извлеченный сигнал искажен"
-    print("Сигнал на выходе демаппера идентичен исходному сигналу на входе")
+    print("Сигнал на выходе демаппера абсолютно идентичен исходному сигналу на входе")
 else:
     print("Ошибка: Приемник не смог выделить временные окна")
